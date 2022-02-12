f9f2d8a7c755c4454b6ca0650737592cf898ce1d
robaldis <robertaldis@gmail.com>
2022-02-12 12:18:35 +0000

    generating dataset

--- README.md
+++ README.md
@@ -10,7 +10,7 @@
 - [[https://www.kaggle.com/karnikakapoor/lyrics|745 lyrics]]
 - [[https://www.kaggle.com/neisse/scrapped-lyrics-from-6-genres?select=lyrics-data.csv|164,790 lyrics]]
 - [[http://millionsongdataset.com/pages/getting-dataset/|300gb, no lyrics]]
-- [[https://data.mendeley.com/datasets/3t9vbwxgr5/2|~28,000]]
+- [[https://data.mendeley.com/datasets/3t9vbwxgr5/2|~28,000]] (done)
 - Manually collect artists I like
 
 
--- gpt-2/generate_dataset.py
+++ gpt-2/generate_dataset.py
@@ -1,10 +1,20 @@
 import pandas as pd
 
 def main():
+    # dataset_1()
+    dataset_2()
+
+def dataset_1():
     data = pd.read_csv('dataset/tcc_ceds_music.csv', header=0)
 
     lyrics = data['lyrics']
-    lyrics.to_csv('dataset/lyrics.csv', index=False, header=False)
+    lyrics.to_csv('dataset/lyrics_1.csv', index=False, header=False)
+
+def dataset_2():
+    data = pd.read_csv('dataset/Songs.csv', header=0)
+
+    lyrics = data['Lyrics']
+    lyrics.to_csv('dataset/lyrics_2.csv', index=False, header=False)
 
 
 if __name__ == "__main__":
